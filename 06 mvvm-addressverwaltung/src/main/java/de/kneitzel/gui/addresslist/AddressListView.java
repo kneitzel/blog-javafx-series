c5c0043b995699a6cd1cb201b017ce9bde859d80
Konrad Neitzel <konrad@kneitzel.de>
2020-10-20 18:26:21 +0200

    Added maven hello world and minor work on 06.

---
+++ 06 mvvm-addressverwaltung/src/main/java/de/kneitzel/gui/addresslist/AddressListView.java
@@ -0,0 +1,15 @@
+package de.kneitzel.gui.addresslist;
+
+import de.saxsys.mvvmfx.FxmlView;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddressListView implements FxmlView<AddressListViewModel>, Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ 06 mvvm-addressverwaltung/src/main/java/de/kneitzel/gui/addresslist/AddressListViewModel.java
@@ -0,0 +1,6 @@
+package de.kneitzel.gui.addresslist;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+public class AddressListViewModel implements ViewModel {
+}
