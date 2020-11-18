9336fec6410f94a96cb9b69b4ea8f8605af37dd0
Konrad Neitzel <konrad@kneitzel.de>
2020-11-18 13:33:46 +0100

    Added Comments to source.

--- 05 helloworld-fxml/src/main/java/helloworld/HelloWorld.java
+++ 05 helloworld-fxml/src/main/java/helloworld/HelloWorld.java
@@ -8,12 +8,37 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Application Klasse, die notwendig ist in jedem JavaFX Programm.
+ * <p>
+ *     Enthält die main Methode und überschreibt mindestens start,
+ * </p>
+ */
 public class HelloWorld extends Application {
 
+    /**
+     * Der Einstiegspunkt für unser Programm.
+     * <p>
+     *     In einer JavaFX Applikation reicht in der Regel ein launch Aufruf.
+     * </p>
+     * @param args Kommandozeilen Argumente.
+     */
     public static void main(final String[] args) {
         launch(args);
     }
 
+    /**
+     * Überschriebene Start methode, die von der JavaFX Applikation mit dem Hauptfenster aufgerufen wird.
+     * <p>
+     *     Sollte dem Hauptfenster ein Inhalt geben und dann das Fenster sichtbar machen.
+     * </p>
+     * <p>
+     *     Wir laden unsere fxml Datei. Der FXMLLoader erstellt dabei auch automatisch die Instanz des Controllers, der
+     *     in der fxml Datei angegeben wurde.
+     * </p>
+     * @param primaryStage Das Hauptfenster.
+     * @throws IOException Da Inhalte geladen werden könnten (fxml Datei, Bilder, ...) kann diese Methode eine IOException werfen.
+     */
     @Override
     public void start(final Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("HelloWorld.fxml"));
@@ -21,5 +46,4 @@ public class HelloWorld extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 }
--- 05 helloworld-fxml/src/main/java/helloworld/HelloWorldController.java
+++ 05 helloworld-fxml/src/main/java/helloworld/HelloWorldController.java
@@ -6,19 +6,61 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 
+/**
+ * Controller Klasse für das HelloWorld Fenster.
+ * <p>
+ *     Das Fenster ist in HelloWorld.fxml beschrieben. In FXML Datei wird diese Klasse als Controller angegeben,
+ *     so daß die Instanz automatisch erzeugt wird.
+ * </p>
+ * <p>
+ *     Es wird von uns selbst keine Instanz dieses Controllers erzeugt!
+ * </p>
+ */
 public class HelloWorldController {
 
+    /**
+     * "greet me" Knopf
+     * <p>
+     *     Dieser wird automatisch nach der Erzeugung der Instanz vom FXML Loader gesetzt.
+     * </p>
+     */
     @FXML private Button greetMeButton;
 
+    /**
+     * CloseApplicationAction
+     * <p>
+     *     Diese Methode beendet die Anwendung. Durch den ActionEvent Parameter hat die Methode die
+     *     Signatur, die notwendig ist, damit die Methode im FXML beim close Knopf eingetragen werden kann.
+     * </p>
+     * @param e ActionEvent Parameter.
+     */
     public void closeApplicationAction(final ActionEvent e) {
         System.exit(0);
     }
 
+    /**
+     * Methode zum Anzeigen eines Grusses.
+     * <p>
+     *     Mit dieser Methode wird das hinzufügen eines ActionListeners im Code gezeigt.
+     *     (Siehe initialize Methode!)
+     * </p>
+     * @param e ActionEvent Parameter.
+     */
     public void handleGreetMeButton(final ActionEvent e) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "Hallo Anwender!", ButtonType.OK);
         alert.showAndWait();
     }
 
+    /**
+     * Initialisierung des Controllers.
+     * <p>
+     *     Durch die @FXML Annotation wird diese Methode automatisch aufgerufen, nachdem die Instanz erzeugt und die
+     *     Instanzvariablen, die eine @FXML Annotation haben, gesetzt sind.
+     * </p>
+     * <p>
+     *     Wir setzen den ActionHandler für den greetMeButton auf die handleGreetMeButton Methode.
+     * </p>
+     */
     @FXML
     public void initialize() {
         greetMeButton.setOnAction(this::handleGreetMeButton);
